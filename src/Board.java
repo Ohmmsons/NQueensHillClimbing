9745de3ddea5eac87b57c46dbb030dce82d469c0
Ohm <ohmmsons@gmail.com>
2022-11-11 16:17:23 +0000

    Board.java updates

--- src/Board.java
+++ src/Board.java
@@ -3,10 +3,12 @@ import java.util.*;
 class Board implements Ilayout, Cloneable {
 
     int n;
-    boolean[][] board;
+    boolean[][] board; // Ocupado = true
     int[] cols;
     int[] ldiags;
     int[] rdiags;
+
+    Random r = new Random();
 
     public Board(int m) throws IllegalStateException { //Simulates a two dimensional board using a String by filling in the empty spaces with " "
         n = m;
@@ -15,6 +17,19 @@ class Board implements Ilayout, Cloneable {
         ldiags = new int[n*2-1];
         rdiags = new int[n*2-1];
         //CREATE RANDOM BOARD WITH NO QUEENS ON THE SAME AND INITIALIZE COLS LDIAGS AND RDIAGS
+        for(int i = 0; i<n; i++){
+            board[i][r.nextInt(n)] = true;
+        }
+        for(int i = 0; i<cols.length; i++){
+            for(int j = 0; j<n; j++)
+                if(board[i][j]) cols[j]++;
+        }
+        for(int i = 0; i<ldiags.length; i++){
+            //Calcular rainhas em cada diagonal esquerda
+        }
+        for(int i = 0; i< rdiags.length; i++){
+            //Calcular rainhas em cada diagonal direita
+        }
     }
 
     public String toString() {
@@ -26,18 +41,18 @@ class Board implements Ilayout, Cloneable {
     }
 
     public Ilayout getRandomSuccessor() {
-        //MOVE A RANDOM QUEEN TO A RANDOM UNOCCUPIED SQUARE
+        //MOVE A RANDOM QUEEN TO A RANDOM UNOCCUPIED SQUARE AND UPDATE NCONFLICTS
         return null;
     }
 
     public int getConflicts() {
-        int result = 0;
+        int nconflicts = 0;
         for(int i = 0;i < n; i++){
-            if(cols[i]>1) result++;
-            if(rdiags[i]>1) result++;
-            if(ldiags[i]>1) result++;
+            if(cols[i]>1) nconflicts++;
+            if(rdiags[i]>1) nconflicts++;
+            if(ldiags[i]>1) nconflicts++;
         }
-        return result;
+        return nconflicts;
     }
 
     public Board clone() {
